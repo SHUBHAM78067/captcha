5c09d7de2389e9c8db5a57451602b2a3f7fdadf4
Shubham Sharma <shubham78067@gmail.com>
2023-04-12 12:10:20 +0530

    theme change done on switch button

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("captcha.captcha.controller.View1", {
+            onInit: function () {
+                setInterval(this.darkMode.bind(this),1000);
+
+            },
+            darkMode: function(oEvent){
+                debugger;
+               var oState = this.getView().byId("dlid").getState();
+               if(oState === true){
+                sap.ui.getCore().applyTheme("sap_horizon_dark");
+               }else{
+                sap.ui.getCore().applyTheme("sap_bluecrystal");
+               };
+              
+            }
+        });
+    });
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["captcha/captcha/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
